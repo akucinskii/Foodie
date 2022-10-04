893de0b8dc83fd38097fc44401b100313f996c12
Artur Kucinski <artuyees@gmail.com>
2022-10-05 00:09:25 +0200

    Hard-coded button color to yellow

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,19 +6,11 @@ type Props = {
   children: React.ReactNode;
 };
 
-const Button = ({ onClick, color, children }: Props) => {
+const Button = ({ onClick, children }: Props) => {
   return (
     <button
       type="button"
-      className={`inline-block rounded bg-${
-        color ?? "blue"
-      }-600 px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-${
-        color ?? "blue"
-      }-700 hover:shadow-lg focus:bg-${
-        color ?? "blue"
-      }-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-${
-        color ?? "blue"
-      }-800 active:shadow-lg`}
+      className={`sm:text:sm md:text-md inline-block rounded bg-yellow-500 px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-yellow-600 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-lg`}
       onClick={onClick}
     >
       {children}

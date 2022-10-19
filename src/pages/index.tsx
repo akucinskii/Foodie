01e9dca4f41d8ff4a71a6ee19384cc177bd4ce75
Artur Kucinski <artuyees@gmail.com>
2022-10-20 00:47:35 +0200

    Cleaned up index html

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useSession, signIn, signOut } from "next-auth/react";
-import { useEffect } from "react";
-import Image from "next/image";
 
 const Home: NextPage = () => {
-  const { data: session } = useSession();
-
-  useEffect(() => {
-    console.log(session);
-  }, [session]);
-
   return (
     <>
       <Head>
@@ -30,61 +21,17 @@ const Home: NextPage = () => {
           Tutorial
         </h2>
 
-        {/* <h2 className="text-center text-lg font-bold">
-          {" "}
-          You want Mcdonalds (You have to create the list)
-        </h2>
         <ol className="list-decimal">
-          <li>
-            Click on <span className="underline">New order</span>
-          </li>
-          <li> Enter your name and Name of your order list</li>
-          <li> Click on button to submit</li>
-          <li>
-            Done, your order has been created.
-            <br /> follow next steps to add your products
-          </li>
-        </ol> */}
-        {/* <h3 className="text-center text-lg font-bold">
-          You want McDonalds
-        </h3> */}
-        <ol className="list-decimal">
+          <li>Login first</li>
           <li>
             Click on <span className="underline">All orders</span> on the navbar
           </li>
+
           <li> Select newest Order</li>
           <li> Add items that you want</li>
           <li> Click submit and you are Done</li>
         </ol>
       </div>
-      {session ? (
-        <>
-          <h1 className="text-center text-2xl font-bold">
-            Welcome {session?.user?.name}
-            {session && session.user && session.user.image && (
-              <Image
-                src={session?.user?.image}
-                alt="text"
-                width={50}
-                height={50}
-              />
-            )}
-          </h1>
-          <button
-            className="rounded bg-yellow-500 py-2 px-4 font-bold text-white"
-            onClick={() => signOut()}
-          >
-            Sign out
-          </button>
-        </>
-      ) : (
-        <button
-          className="rounded bg-yellow-500 py-2 px-4 font-bold text-white"
-          onClick={() => signIn()}
-        >
-          Sign in
-        </button>
-      )}
     </>
   );
 };

21b3c9bd25d88c79a847d8d216cd8844aa04807b
Artur Kucinski <artuyees@gmail.com>
2022-10-20 23:57:21 +0200

    Updated types

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -7,6 +7,16 @@ declare module "next-auth" {
   interface Session {
     user?: {
       id: string;
+      name: string;
+      email: string;
+      image: string | null;
     } & DefaultSession["user"];
+  }
+
+  interface User {
+    id: string;
+    name: string;
+    email: string;
+    image: string | null;
   }
 }

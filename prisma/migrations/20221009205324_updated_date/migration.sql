3d0bcbf3e84fc11e93dc859e22b8bd0d5f5ccdd7
Artur Kucinski <artuyees@gmail.com>
2022-10-31 22:13:31 +0100

    Removed old migrations

--- prisma/migrations/20221009205324_updated_date/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Order" ALTER COLUMN "createdAt" SET DATA TYPE TIMESTAMP(3);
--- prisma/migrations/20221026190905_random_number/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "randomNumber" (
-    "id" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "number" INTEGER NOT NULL,
-
-    CONSTRAINT "randomNumber_pkey" PRIMARY KEY ("id")
-);

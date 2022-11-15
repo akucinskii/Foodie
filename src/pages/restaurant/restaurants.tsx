9e6be949336c279aa507be63cc2a5ed5b885d9cb
Artur Kucinski <artuyees@gmail.com>
2022-11-16 00:03:04 +0100

    Added title to restaurants page

--- src/pages/restaurant/restaurants.tsx
+++ src/pages/restaurant/restaurants.tsx
@@ -8,8 +8,8 @@ const restaurants = () => {
   const restaurants = trpc.restaurant.getAllRestaurants.useQuery();
   return (
     <Wrapper>
+      <h1 className="text-4xl font-bold text-base-content">Restaurants</h1>
       <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="sr-only">Products</h2>
         <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {restaurants.data?.map((restaurant) => (
             <Link
